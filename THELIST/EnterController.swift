07689b47cd5ded389e85cca36fc6385a40b872ce
felos9001 <90056233+Felos9001@users.noreply.github.com>
2023-03-07 18:31:32 -0500

    initial commit

---
+++ THELIST/EnterController.swift
@@ -0,0 +1,41 @@
+//
+//  StartupController.swift
+//  THELIST
+//
+//  Created by Felipe Solorzano on 3/7/23.
+//
+
+import UIKit
+
+class EnterController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var f: UITextField!
+    
+    var update: (() -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        f.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveList))
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        saveList()
+        return true
+    }
+    
+    @objc func saveList() {
+        guard let text = f.text, !text.isEmpty else {
+            return 
+        }
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        let count2 = count + 1
+        UserDefaults().set(count2, forKey: "count" )
+        UserDefaults().set(count2, forKey: "list_\(count2)")
+        
+        update?()
+        navigationController?.popViewController(animated: true)
+    }
+}
--- THELIST/ViewController.swift
+++ THELIST/ViewController.swift
@@ -8,12 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var t: UITableView!
+    var lists = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "ToDoList"
+        t.delegate = self
+        t.dataSource = self
+        if UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count1")
+        }
+        updateList()
     }
-
-
+    
+    func updateList() {
+        lists.removeAll()
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        for i in 0..<count {
+            if let list = UserDefaults().value(forKey: "list_\(i+1)") as? String {
+                lists.append(list)
+            }
+        }
+        
+        t.reloadData()
+    }
+    
+    @IBAction func NewList() {
+        let viewC = storyboard?.instantiateViewController(withIdentifier: "start") as! EnterController
+        viewC.title = "new list"
+        viewC.update = {
+            DispatchQueue.main.async {
+                    self.updateList()
+            }
+        }
+        navigationController?.pushViewController(viewC, animated: true)
+    }
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let viewC = storyboard?.instantiateViewController(withIdentifier: "list") as! ListViewController
+        viewC.title = "new list"
+        viewC.list = lists[indexPath.row]
+            navigationController?.pushViewController(viewC, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lists.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let c = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        c.textLabel?.text = lists[indexPath.row]
+        return c
+    }
+}
